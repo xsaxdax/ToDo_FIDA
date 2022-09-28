b04034434de41c4233cb7f0acad2aec946b9fddd
xsaxdax <60689251+xsaxdax@users.noreply.github.com>
2022-09-28 15:31:56 +0200

    local storage done

---
+++ components/Dialog.vue
@@ -0,0 +1,39 @@
+<template>
+
+             <v-dialog
+          v-model="dialog"
+          max-width="500px"
+        >
+          <v-card>
+            <v-card-text>
+              <v-text-field label="ToDo"></v-text-field>
+                <v-date-picker v-model="picker"></v-date-picker>
+
+              <small class="grey--text">* This doesn't actually save.</small>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn
+                text
+                color="primary"
+                @click="dialog = false"
+              >
+                Submit
+                {{reversedMessage}}
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>          
+</template>
+
+        <script>
+    export default {
+  name: "Dialog",
+  props:{
+    dialog
+  }
+}
+</script>
+
